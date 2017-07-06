1a51f86c7543ba665aa90a45208737514c6ebc07
Andrea Brandi <me@stari.se>
2017-07-06 14:50:56 +0200

    Disqus thread css

--- functions.php
+++ functions.php
@@ -9,7 +9,7 @@
 add_theme_support('solero-jquery-cdn');
 add_theme_support('solero-clean-up');
 add_theme_support('solero-nav-walker');
-add_theme_support('solero-relative-urls');
+// add_theme_support('solero-relative-urls');
 add_theme_support('solero-js-to-footer');
 add_theme_support('solero-disable-trackbacks');
 add_theme_support('solero-disable-asset-versioning');
